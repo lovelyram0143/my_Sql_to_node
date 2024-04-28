d49a39856408f5ac7afc07d9637ed75d1bbd74c8
Ramesh Bojanapu <84425972+lovelyram0143@users.noreply.github.com>
2024-04-28 12:05:53 +0530

    update delete

--- routes/programminglang.js
+++ routes/programminglang.js
@@ -24,6 +24,25 @@ router.post('/add', async function(req, res, next) {
     next(err);
   }
 });
+/* PUT programming language */
+router.put('/update/:id', async function(req, res, next) {
+  try {
+    res.json(await programmingLanguages.update(req.params.id, req.body));
+  } catch (err) {
+    console.error(`Error while updating programming language`, err.message);
+    next(err);
+  }
+});
+
+/* DELETE programming language */
+router.delete('/delete/:id', async function(req, res, next) {
+  try {
+    res.json(await programmingLanguages.remove(req.params.id));
+  } catch (err) {
+    console.error(`Error while deleting programming language`, err.message);
+    next(err);
+  }
+});
 
 
 module.exports = router;
--- services/programminglang_perpage.js
+++ services/programminglang_perpage.js
@@ -34,8 +34,41 @@ async function create(programmingLanguage){
   return {message};
 }
 
+async function update(id, programmingLanguage){
+  const result = await db.query(
+    `UPDATE programming_languages 
+    SET name="${programmingLanguage.name}", released_year=${programmingLanguage.released_year}, githut_rank=${programmingLanguage.githut_rank}, 
+    pypl_rank=${programmingLanguage.pypl_rank}, tiobe_rank=${programmingLanguage.tiobe_rank} 
+    WHERE id=${id}` 
+  );
+
+  let message = 'Error in updating programming language';
+
+  if (result.affectedRows) {
+    message = 'Programming language updated successfully';
+  }
+
+  return {message};
+}
+
+async function remove(id){
+  const result = await db.query(
+    `DELETE FROM programming_languages WHERE id=${id}`
+  );
+
+  let message = 'Error in deleting programming language';
+
+  if (result.affectedRows) {
+    message = 'Programming language deleted successfully';
+  }
+
+  return {message};
+}
+
 module.exports = {
   getMultiple,
-  create
+  create,
+  update,
+  remove
 }
 
